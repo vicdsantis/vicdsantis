92f026b3f97f25dbb07fe55c2532e1eb47820ee1
Victoria De Santis <162721946+vicdsantis@users.noreply.github.com>
2024-03-27 16:59:37 -0300

    entrega change.py

--- change.py
+++ change.py
@@ -1,3 +1,12 @@
 def change():
     expense = 23.75
     money = 100
+    print(f"Ingresar gasto:")
+    print(f"{expense}")
+    print(f"Dinero recibido")
+    print(f"{money}\n")
+    print(f"{"Vuelto"}\n")
+    print(f"{"Pesos:"}\n")
+    print(f"{int(money - expense)}")
+    print(f"Centavos:")
+    print(f"{int(money % expense)**2}")
