90f372c00a3c113e5e68807a67d89ab748168f28
Victoria De Santis <162721946+vicdsantis@users.noreply.github.com>
2024-03-27 17:03:04 -0300

    Entrega earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,5 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    print(f"The result of {x} comes first in the dictionary than {y} is {x < y}")
+    print(f"The result of {x} comes first in the dictionary than {y} is {x < y}")
