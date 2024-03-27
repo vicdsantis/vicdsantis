9edad93a06621ee4e0b07f3d911919acff6c5697
Victoria De Santis <162721946+vicdsantis@users.noreply.github.com>
2024-03-27 11:28:11 -0300

    entrega ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,7 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    print(f"{first_name}".lower()+ " " + f"{last_name}".lower())
+    print(f"{first_name}".title()+ " " + f"{last_name}".title())
+    print(f"{first_name}".upper()+ " " + f"{last_name}".upper())
+    print(f"\t{first_name}".lower()+ " " + f"{last_name}".lower())
